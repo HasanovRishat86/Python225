1b0c7f6b8edc970bd4a59a12763464dd3e97c554
HasanovRishat86 <HasanovRishat@Gmail.com>
2022-09-04 22:03:36 +0500

    повторение change

--- Class work/main.py
+++ Class work/main.py
@@ -1428,11 +1428,11 @@
 # a = {'A' + i[1:] if i[0] == 'a' else 'B' + i[1:] for i in r if i[1] == 'c'}
 # print(a)
 
-print('Вносим изменения')
+# print('Вносим изменения')
 
 print('Вносим изменения в склонированный проект')
 
-print('PyCharm')
+# print('PyCharm')
 
 
 #                                             Занятие № 11
@@ -1445,21 +1445,21 @@ print('PyCharm')
 
 #                                             Занятие № 13
 
-# x = {'a': 1, 'b': 2}
-# y = {'b': 3, 'c': 4}
-# # z = x.copy()
-# # z.update(y)
-# z = x | y
-# print(z)
+x = {'a': 1, 'b': 2}
+y = {'b': 3, 'c': 4}
+# z = x.copy()
+# z.update(y)
+z = x | y
+print(z)
 
-# d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
-#
-# new_d = dict()
-# new_d['name'] = d.pop('name')
-# new_d['salary'] = d.pop('salary')
-#
-# print(d)
-# print(new_d)
+d = {'name': 'Kelly', 'age': 25, 'salary': 8000, 'city': 'New York'}
+
+new_d = dict()
+new_d['name'] = d.pop('name')
+new_d['salary'] = d.pop('salary')
+
+print(d)
+print(new_d)
 
 
 
