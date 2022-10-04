712b9af213c5658092d54e6d8d176302eea058a2
HasanovRishat86 <HasanovRishat@Gmail.com>
2022-10-04 18:55:47 +0500

    ДЗ 15

---
+++ Home Work/Home Work_15/main.py
@@ -0,0 +1,46 @@
+# Задача № 1
+
+# print((lambda x, y, z: x * y * z)(2, 5, 5))
+
+# Задача № 2
+
+# students = [
+#     {'name': 'Jennifer', 'final': 95},
+#     {'name': 'David', 'final': 92},
+#     {'name': 'Nikolas', 'final': 98},
+# ]
+#
+# res1 = sorted(students, key=lambda item: item['name'])
+# print(res1)
+#
+# res2 = sorted(students, key=lambda item: item['final'], reverse=True)
+# print(res2)
+
+# Задача № 3
+
+# students = [
+#     {'name': 'Jennifer', 'final': 95},
+#     {'name': 'David', 'final': 92},
+#     {'name': 'Nikolas', 'final': 98},
+# ]
+#
+# res1 = max(students, key=lambda item: item['final'])
+# print(res1)
+#
+# res2 = min(students, key=lambda item: item['final'])
+# print(res2)
+
+# Задача № 4
+
+# def kvadrat(x):
+#     return x ** 2
+#
+#
+# def kub(x):
+#     return x ** 3
+#
+# nums = [3, 5, 7, 3, 9, 5, 7, 2]
+# a = list(map(kvadrat, nums))
+# print(a)
+# b = list(map(kub, nums))
+# print(b)
